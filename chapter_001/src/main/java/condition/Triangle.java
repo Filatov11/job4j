9ae81a66e4df4f5ff86a70e7b3e9c26b448124c1
Filatov11 <denfilatov@protonmail.com>
2019-09-24 22:06:28 +0300

    triangle

--- chapter_001/src/main/java/condition/Triangle.java
+++ chapter_001/src/main/java/condition/Triangle.java
@@ -1,4 +1,11 @@
 package condition;
 
 public class Triangle {
+    public static boolean exist(double ab, double bc, double ac) {
+        if ( ((ab + ac )  > bc ) && (( ac + bc) > ab) && ((ab + ab + bc) > ac )  )
+        { return true; }
+        else
+       { return false; }
+
+    }
 }
