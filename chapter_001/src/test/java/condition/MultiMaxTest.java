9ca3d714d3914d1144d6f5d12c2a993dac7f407d
Filatov11 <denfilatov@protonmail.com>
2019-09-24 23:05:53 +0300

    multimax

---
+++ chapter_001/src/test/java/condition/MultiMaxTest.java
@@ -0,0 +1,26 @@
+package condition;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+public class MultiMaxTest {
+    @Test
+    public  void  whenThirdMax() {
+        MultiMax check = new MultiMax();
+        int  result = check.max(1, 2, 4);
+        assertThat(result, is(4));
+    }
+    @Test
+    public  void  whenSecondMax() {
+        MultiMax check = new MultiMax();
+        int  result = check.max(2, 7, 4);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public  void  whenFirstMax() {
+        MultiMax check = new MultiMax();
+        int  result = check.max(11, 7, 4);
+        assertThat(result, is(11));
+    }
+
+}
