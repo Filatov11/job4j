e3cf39d6ec95f7580ce3a384ff41ac3c1279c33e
Filatov11 <denfilatov@protonmail.com>
2019-09-25 23:33:19 +0300

    sqarea

---
+++ chapter_001/src/main/java/condition/SqMax.java
@@ -0,0 +1,28 @@
+package condition;
+
+public class SqMax {
+    public static int max(int first, int second, int third, int forth) {
+        int result = forth;
+        if (first > second) {
+            if (first > third) {
+                if (first > forth) {
+                    result = first;
+                }
+            }
+        }
+        if (second > third ) {
+            if (second > forth )  {
+                if (second > third ) {
+                    result = second;
+                }
+            }
+        }
+        if (third > second) {
+            if ( third > first) {
+                if (third > forth)
+                {result = third;}
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/condition/SqMaxTest.java
@@ -0,0 +1,44 @@
+package condition;
+import org.junit.Test;
+import condition.SqMax;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+public class SqMaxTest {
+
+
+    @Test
+    public  void qrtest1()  {
+        int k = 1; int p = 4; int m = 2; int n = 10;
+        int expected = 10;
+        int s = condition.SqMax.max(k, p, m, n);
+        assertThat(s, is(expected));
+    }
+
+    @Test
+    public  void qrtest2()  {
+        int k = 1; int p = 4; int m = 2; int n = 10;
+        int expected = 10;
+        int s = condition.SqMax.max(p, m, n, k);
+        assertThat(s, is(expected));
+    }
+
+    @Test
+    public  void qrtest3()  {
+        int k = 1; int p = 4; int m = 2; int n = 10;
+        int expected = 10;
+        int s = condition.SqMax.max(m, n, k, p);
+        assertThat(s, is(expected));
+    }
+
+
+    @Test
+    public  void qrtest4()  {
+        int k = 1; int p = 4; int m = 2; int n = 10;
+        int expected = 10;
+        int s = condition.SqMax.max(n, k, p, m);
+        assertThat(s, is(expected));
+    }
+
+
+
+}
