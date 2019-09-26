d47842be81a936c8ed4af032de9dd6c6bd871400
Filatov11 <denfilatov@protonmail.com>
2019-09-26 22:24:24 +0300

    mortage

---
+++ chapter_001/src/test/java/ru/job4/loop/MortageTest.java
@@ -0,0 +1,24 @@
+package ru.job4.loop;
+import org.junit.Test;
+import ru.job4j.loop.Mortage;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+public class MortageTest {
+
+
+
+        @Test
+        public void when1Year() {
+            Mortage mortgage = new Mortage();
+            int year = mortgage.year(1000, 1200, 1);
+            assertThat(year, is(1));
+        }
+
+        @Test
+        public void when2Year() {
+            Mortage mortgage = new Mortage();
+            int year = mortgage.year(100, 120, 50);
+            assertThat(year, is(2));
+        }
+    }
+
