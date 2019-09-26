bfbebf80451498503fd05656060b1ac831f50636
Filatov11 <denfilatov@protonmail.com>
2019-09-26 21:36:37 +0300

    fitness

---
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+public class Fitness {
+
+
+    public int calc(int ivan, int nik) {
+        int month = 1;
+        int weightIvan = ivan;
+        int weightNic = nik;
+        if (weightIvan > weightNic)  {
+            month = 0; return month;
+        } else {  month = 1;
+            while (weightIvan < weightNic) {
+
+                weightIvan = ivan * month;
+                weightNic = nik * month;
+                month = month + 1;
+            }
+        }
+            return month;
+        }
+
+
+    }
