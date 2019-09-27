eebeeb0dd4f00147f5b7347e2e041ff62edaaff0
Filatov11 <denfilatov@protonmail.com>
2019-09-28 01:02:22 +0300

    Square

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class Square {
+    public int[] calculate(int bound) {
+        int[] rst = new  int[bound];
+        int val;
+        for (int i = 1; i <= (bound) ; i++ ) {
+            val = i;
+            System.out.println(val);
+            rst[i-1] = val * val;
+        }
+        return rst;
+    }
+}
