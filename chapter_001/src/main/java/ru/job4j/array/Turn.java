2e7d0ded7f932a60704dad255d83f95a5ddf18b1
Filatov11 <denfilatov@protonmail.com>
2019-09-30 00:48:13 +0300

    Turn

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class Turn {
+    public int[] back(int[] array) {
+        int temp ;
+        for (int i = 0 ; i  <=  ((array.length-1)/2) ; i++) {
+            temp = array[((array.length-1) -i)] ;
+            array[((array.length-1) -i)] =  array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
+}
