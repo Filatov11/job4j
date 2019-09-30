0b37514c41a289f2bf48e206d54348d33a6ab5d1
Filatov11 <denfilatov@protonmail.com>
2019-09-30 22:02:14 +0300

    Check

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class Check {
+    boolean etalon;
+
+    public boolean mono(boolean[] data) {
+        etalon = data[0];
+        boolean result = false;
+        for (int i = 1; i < data.length; i++) {
+
+            if (etalon != data[i]) {
+                result = false;
+                break;
+            }
+            else   result = true;
+        }
+
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4/loop/array/CheckTest.java
@@ -0,0 +1,22 @@
+package ru.job4.loop.array;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import  ru.job4j.array.Check;
+public class CheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+}
