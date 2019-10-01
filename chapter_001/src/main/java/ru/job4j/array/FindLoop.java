4cc5d25f4f4b2e974031d1f1969b062017bd415d
Filatov11 <denfilatov@protonmail.com>
2019-10-02 01:13:00 +0300

    FindLoop 6.5

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,15 +1,26 @@
 package ru.job4j.array;
 
 public class FindLoop {
-    public  int indexOf (int[]data, int el ) {
+    public static  int indexOf(int[] data, int el) {
         int rst = -1;
-        for ( int i = 0; i  <  (data.length-1) ; i++)  {
+        for (int i = 0; i < (data.length - 1); i++) {
             if (data[i] == el) {
                 rst = i;
                 break;
             }
         }
-        return  rst;
+        return rst;
     }
-}
 
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int i = start; i < finish; i++) {
+            if (data[i] == el) {
+                rst = i;
+                break;
+            }
+        }
+        return rst;
+    }
+
+}
