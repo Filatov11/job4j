732e8cd6f9a13637d43d3dc2fc0731f72832be61
Filatov11 <denfilatov@protonmail.com>
2019-10-03 01:38:31 +0300

    Defragment

---
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            String cell = array[index];
+            if (cell == null) {
+                int ind = index;
+                String tempString = null;
+                while ((ind < array.length-1) && (tempString == null)) {
+                    tempString = array[ind];
+                    array[ind] = array[ind+1];
+                    array[ind+1] = tempString;
+                    ind ++;
+                    //  if (tempString == null)
+              }
+
+
+                // переместить первую не null ячейку
+                // Здесь нужен цикл while
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
