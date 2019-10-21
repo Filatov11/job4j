661949f5864a7b54440df3fd34f16ab02b6fa9e3
Filatov11 <denfilatov@protonmail.com>
2019-10-21 22:56:03 +0300

    ArrayChar1

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,13 +3,13 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsW(char[] word, char[] pref) {
         boolean result = false;
-        /*
-        if (word.length > pref.length) {
-            minLength = pref.length;
-          } else {
-             minLength = word.length;
-        / }
-        */
+        //
+        // if (word.length > pref.length) {
+        //     minLength = pref.length;
+        //   } else {
+        //      minLength = word.length;
+        //}
+        //
         int minLength = (word.length > pref.length) ? pref.length : word.length;
         for (int i = 0 ; i < minLength; i++ )  {
             if (word[i] == pref[i]) {
