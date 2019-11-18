e1d95c17863ac905c43f8121c33e841db1b0186e
Filatov11 <denfilatov@protonmail.com>
2019-11-18 22:57:14 +0300

    Jukebox1

--- chapter_002/src/main/java/oop/Jukebox.java
+++ chapter_002/src/main/java/oop/Jukebox.java
@@ -3,9 +3,9 @@ package oop;
 public class Jukebox {
     public  void  music(int position) {
         if (position == 1)
-        System.out.println("Пусть бегут неуклюже");
-         else if (position == 2) System.out.println("Спокойной ночи");
-                else System.out.println("нет песни: 1 или 2 ");
+        {System.out.println("Пусть бегут неуклюже");}
+         else if (position == 2) {System.out.println("Спокойной ночи");}
+                else {System.out.println("нет песни: 1 или 2 ");}
     }
 
 
