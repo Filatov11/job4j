06a2fc9e8fcfa986206cc6e3bc05727a0f723cbd
Filatov11 <denfilatov@protonmail.com>
2019-11-20 00:32:35 +0300

    Point

--- chapter_001/src/main/java/condition/Triangle.java
+++ chapter_001/src/main/java/condition/Triangle.java
@@ -1,8 +1,45 @@
-package condition;
-
+package ru.job4j.condition;
+import  ru.job4j.condition.Point;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 public class Triangle {
-    public static boolean exist(double ab, double bc, double ac) {
-        return ((ab + ac) > bc) && ((ac + bc) > ab) && ((ab + ab + bc) > ac);
+    private Point first;
+    private Point second;
+    private Point third;
 
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
     }
+    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+        double rsl = -1;
+        Point first = new Point(x1, y1);
+        Point second = new Point(x2, y2);
+        Point third = new Point(x3, y3);
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = perimeter(a, b, c);
+        if (this.exists(a, b, c)) {
+            rsl = Math.sqrt((p *(p - a*b) * (p - a*c) * (p - b*c)));
+
+        }
+        return rsl;
+    }
+
+
+
+    private boolean exists (double a, double b, double c ) {
+        if ((a + b) >= c ) { return true;
+         }
+        else {
+            return false;
+        }
+    }
+    private  double perimeter (double a, double b, double c ){
+
+        return (a + b + c)/2;
+    }
+
 }
--- chapter_001/src/test/java/condition/TriangleTest.java
+++ chapter_001/src/test/java/condition/TriangleTest.java
@@ -1,14 +1,20 @@
 package condition;
 
 import org.junit.Test;
-
+import ru.job4j.condition.Point;
+import  ru.job4j.condition.Triangle;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class TriangleTest {
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+        Point one = new Point(1,2);
+        Point two = new Point(3,4);
+        Point three = new Point(4,5);
+        Triangle triangle = new Triangle(one, two, three);
+        double result = triangle.area(1,2,3,4,4,5);
+      //  boolean result = Triangle.exists(2.0, 2.0, 2.0);
+        assertThat(result, is(3.7461673761871523));
     }
 }
