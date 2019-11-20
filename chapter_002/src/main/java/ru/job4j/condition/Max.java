32e5842cee00fec9e4871ef00f1207ce3f977bc5
Filatov11 <denfilatov@protonmail.com>
2019-11-21 00:24:06 +0300

    MaxOverload

--- chapter_002/src/main/java/ru/job4j/condition/Max.java
+++ chapter_002/src/main/java/ru/job4j/condition/Max.java
@@ -1,5 +1,7 @@
 package ru.job4j.condition;
 
+import javax.swing.*;
+
 public class Max {
     public static int max(int left, int right) {
         int result = left;
@@ -7,14 +9,27 @@ public class Max {
         return result;
     }
     public static int max(int left, int right, int middle) {
-        int maxval = left;
-        if (right > left) {
-              maxval = right; if (maxval < middle) { maxval = middle; return maxval}  else  maxval = right; }
+        int maxval;
+        if ((left > right) && (left > middle)) {
+              maxval = left;}
+        else if ((right > left) && (right > middle)) { maxval = right;}
+        else maxval = middle;
+
          return  maxval;
 
-         else if
+       //  else if
        // result = left < right ? right : left;
-        return result;
+      //  return result;
+    }
+
+    public static int max(int left, int right, int middle, int four) {
+        int maxval;
+        if ((left > right) && (left > middle) && (left > four)) {
+            maxval = left;}
+        else if ((right > left) && (right > middle) && (right > four)) { maxval = right;}
+        else if ((middle > left) && (middle > right) &&  (middle > four)) { maxval = middle;}
+        else maxval = four;
+        return  maxval;
     }
 
 }
---
+++ chapter_002/src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.condition;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+    @Test
+    public void whenMax1To2Then2() {
+        int result = Max.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenMax4To1Then4() {
+        int result = Max.max(4, 1);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenMax5To5Then5() {
+        int result = Max.max(5, 5);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenMax5To5Then51() {
+        int result = Max.max(5, 5, 7);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenMax5To5Then52() {
+        int result = Max.max(5, 5, 7,11);
+        assertThat(result, is(11));
+    }
+
+
+}
+
