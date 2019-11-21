51caa42aa2d7a5996902810ebb4eab019e0fd4b3
Filatov11 <denfilatov@protonmail.com>
2019-11-21 22:22:15 +0300

    MaxOverload1

--- chapter_002/src/main/java/ru/job4j/condition/Max.java
+++ chapter_002/src/main/java/ru/job4j/condition/Max.java
@@ -9,13 +9,15 @@ public class Max {
         return result;
     }
     public static int max(int left, int right, int middle) {
-        int maxval;
-        if ((left > right) && (left > middle)) {
-              maxval = left;}
-        else if ((right > left) && (right > middle)) { maxval = right;}
-        else maxval = middle;
+        int result = left;
+        result = left < max(right,middle) ? max(right,middle) : left;
+        //    int maxval;
+      //  if ((left > right) && (left > middle)) {
+      //        maxval = left;}
+      //  else if ((right > left) && (right > middle)) { maxval = right;}
+     //   else maxval = middle;
 
-         return  maxval;
+         return result ; // maxval;
 
        //  else if
        // result = left < right ? right : left;
@@ -23,13 +25,16 @@ public class Max {
     }
 
     public static int max(int left, int right, int middle, int four) {
-        int maxval;
-        if ((left > right) && (left > middle) && (left > four)) {
-            maxval = left;}
-        else if ((right > left) && (right > middle) && (right > four)) { maxval = right;}
-        else if ((middle > left) && (middle > right) &&  (middle > four)) { maxval = middle;}
-        else maxval = four;
-        return  maxval;
+        int result = left;
+      result = max(right,  middle, four) < max(right,middle) ? max(right,middle) : max(right,  middle, four) ;
+        return result;
+    //    int maxval;
+   //     if ((left > right) && (left > middle) && (left > four)) {
+   //         maxval = left;}
+   //     else if ((right > left) && (right > middle) && (right > four)) { maxval = right;}
+    //    else if ((middle > left) && (middle > right) &&  (middle > four)) { maxval = middle;}
+    //    else maxval = four;
+    //    return  maxval;
     }
 
 }
