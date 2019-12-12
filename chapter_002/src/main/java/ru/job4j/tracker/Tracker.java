46e61f953894c561671a62e45dfdf358913db558
Filatov11 <denfilatov@protonmail.com>
2019-12-12 23:48:58 +0300

    Trackers1

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 import ru.job4j.tracker.Items;
+import java.util.Arrays;
 public class Tracker {
     private final Items[] items = new Items[100];
     private int position = 0;
@@ -23,42 +24,33 @@ public class Tracker {
         Random rm = new Random();
         String str = String.valueOf(rm.nextLong() +"="+ System.currentTimeMillis());
         return str;
-       // int randomInteger = rm.nextInt();
-        //DateFormat dateFormat = new SimpleDateFormat("YYYY_MM_dd_HH_mm_ss");
-        //Date date = new Date();
-       // String stringId = dateFormat.format(date) + randomInteger;
-       // return stringId;
     }
 
 
     public  boolean delete(String id) {
-       boolean res = false;
-        for (int i=0; i < itemss.length; i++ ) {
+        boolean res = false;
+        Items temp = new Items(null);
+        int index = 0;
+        for (int i=0; i < this.position; i++ ) {
             if (itemss[i].getId().equals(id)) {
-                itemss[i] = null;
+                temp = itemss[i];
+                for (int j=i; j < this.position-1; j++ ) {
+                    itemss[j] = itemss[j+1];
+                }
+                itemss[i] = temp;
                 res =  true;
             } else  {res =  false;}
         }
+
         return res;
     }
-   // public boolean replace(String id, Items item) {
-//     boolean res = false;
- //    for (int i=0; i < itemss.length; i++ ) {
-   //      System.out.println(itemss[i].getId());
-   //      if (itemss[i].getId().equals(item.getId())) {
-   //          itemss[i].setName(item.getName());
-   //         res = true;
-  //       } else  {res = false;}
-//     }
- //       return res;
-//    }
 
 
     public boolean replace(String id, Items item) {
         boolean res = false;
-        for (Items i: itemss) {
-            if ((i != null) && (i.getId().equals(item.getId()))) {
-                i.setName(item.getName());
+        for (int i = 0; i < this.position; i++) {
+            if ((this.itemss[i].getId().equals(item.getId()))) {
+               this.itemss[i].setName(item.getName());
                 res = true;
             } else  {res = false;}
         }
@@ -69,51 +61,20 @@ public class Tracker {
 
    //// -----------------
     public Items[] findAll() {
-        int count = 0;
-
-       /// Items[] itemsNotNull = new Items[0];
-        for (int i = 0; i < itemss.length; i++) {
-            if (itemss[i] == null) {
-                count++;
-            }
-        }
-        Items[] itemsNotNull = new Items[count];
-            int cnt = 0;
-            for (int i = 0; i < itemss.length; i++) {
-                if (this.itemss[i] != null) {
-                    itemsNotNull[cnt] = this.itemss[i];
-                    cnt++;
-                }
-            }
-
-        return itemsNotNull;
+        return Arrays.copyOf(this.itemss, this.position);
     }
     //// -----------------
     public Items[] findByName(String key) {
         int cnt = 0;
-        for (int i = 0; i < itemss.length; i++) {
-            if (itemss[i] == null) {
-                cnt++;
-            }
-        }
-     Items[]   itemsSame = new Items[cnt];
-       int cntout = 0;
-        for (int i = 0; i < itemss.length; i++) {
-            if (itemss[i] == null) {
-                itemsSame[cntout]=itemss[i];
-                cntout++;
-            }
-        }
-
-        return itemsSame;
+        Items[] tempItem = new Items[this.position];
+        tempItem = Arrays.copyOf(this.itemss,this.position );
+        return tempItem;
     }
     public Items findById(String id) {
       Items ret = new Items(null);
-     for (int i = 0; i < this.items.length; i++ ) {
-         if ((this.items[i] != null) && ( this.items[i].getName().equals(id))) {
-               System.out.println("findById " + this.items[i].getName());
-              ret = this.items[i];
-
+     for (int i = 0; i < this.position; i++ ) {
+         if ( ( this.itemss[i].getName().equals(id))) {
+              ret = this.itemss[i];
          }
      }
         return ret;
