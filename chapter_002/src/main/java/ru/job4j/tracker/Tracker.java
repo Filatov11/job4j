14ef4842d0308581b263d127b8329eb710cbae55
Filatov11 <denfilatov@protonmail.com>
2019-12-16 01:35:32 +0300

    Trackers3

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -31,7 +31,7 @@ public class Tracker {
         boolean res = false;
         Items temp = new Items(null);
         int index = 0;
-        for (int i=0; i < this.position; i++ ) {
+        for (int i=0; i < this.position-1; i++ ) {
             if (itemss[i].getId().equals(id)) {
                 temp = itemss[i];
                 for (int j=i; j < this.position-1; j++ ) {
@@ -48,11 +48,11 @@ public class Tracker {
 
     public boolean replace(String id, Items item) {
         boolean res = false;
-        for (int i = 0; i < this.position; i++) {
+        for (int i = 0; i < this.position-1; i++) {
             if ((this.itemss[i].getId().equals(item.getId()))) {
                this.itemss[i].setName(item.getName());
                 res = true;
-            } else  {res = false;}
+            }
         }
         return res;
     }
@@ -61,13 +61,25 @@ public class Tracker {
 
    //// -----------------
     public Items[] findAll() {
-        return Arrays.copyOf(this.itemss, this.position);
+        int cnt = 0;
+        for(int i = 0; i < this.position-1; i++){
+            if((this.itemss[i].getId().equals(null))) {
+                cnt++;
+                for(int j = i; j < this.position - 1; j++){
+                    this.itemss[j] = this.itemss[j+1];
+                }
+                break;
+            }  else  { }
+        }
+        Items[] tempItem = new Items[this.position];
+        tempItem = Arrays.copyOf(this.itemss,cnt);
+        return tempItem;
     }
     //// -----------------
     public Items[] findByName(String key) {
         int cnt = 0;
         Items[] temp = new Items[this.position];
-        for (int i = 0; i < this.position; i++) {
+        for (int i = 0; i < this.position-1; i++) {
             if ((this.itemss[i].getId().equals(key))) {
                 temp[cnt] = this.itemss[i];
                 cnt++;
@@ -79,10 +91,11 @@ public class Tracker {
     }
     public Items findById(String id) {
       Items ret = new Items(null);
-     for (int i = 0; i < this.position; i++ ) {
+     for (int i = 0; i < this.position-1; i++ ) {
          if ( ( this.itemss[i].getName().equals(id))) {
               ret = this.itemss[i];
          }
+         break;
      }
         return ret;
     }
