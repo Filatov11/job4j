1cda5cd030f02514940755e0bd123d67efd2b50d
Filatov11 <denfilatov@protonmail.com>
2020-01-01 01:29:21 +0300

    [#179514]_Cheked

--- chapter_002/src/main/java/ru/job4j/io/MagicBall.java
+++ chapter_002/src/main/java/ru/job4j/io/MagicBall.java
@@ -6,17 +6,25 @@ public class MagicBall {
         System.out.println("What you want to now?");
         Scanner str = new Scanner(System.in);
         Scanner yn = new Scanner(System.in);
-        boolean flag=true;
+        boolean flag = true;
         while (flag) {
             System.out.println("I'm a great oracle. What you want to now?");
             String question = str.nextLine();
             int answer = new Random().nextInt(3);
-            if (answer == 0) {System.out.println("Yes. it will be!");}
-             else if (answer == 1) {System.out.println("No. it won't be!");}
-              else {System.out.println("May be yes or may be no...");}
+            if (answer == 0) {
+                System.out.println("Yes. it will be!");
+            } else if (answer == 1) {
+                 System.out.println("No. it won't be!");
+             } else {
+                  System.out.println("May be yes or may be no...");
+              }
             System.out.println("something else?  (yes/no)");
             String ansYN = yn.nextLine().trim().toLowerCase();
-            if (ansYN.equals("yes")) { flag = true;} else { flag = false;}
+            if (ansYN.equals("yes")) {
+                flag = true;
+            } else {
+                flag = false;
+            }
         }
 
     }
