385802c35451ee90ddb634ee9e38de2da2de0618
Filatov11 <denfilatov@protonmail.com>
2020-01-16 00:46:36 +0300

    5.1. Тестирование. Подготовка данных. [#182960]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -15,4 +15,37 @@ public class StartUITest {
         Items expected = new Items("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
-}
+
+    @Test
+    public void whenReplaceItem() {
+        String[] answers1 = {"Fix Printer"};
+        Input input1 = new StubInput(answers1);
+        String[] answers2 = {"Fix Scanner"};
+        Input input2 = new StubInput(answers2);
+        Tracker trackerR = new Tracker();
+        StartUI.createItem(input1, trackerR);
+        Items edited = trackerR.findAll()[0];
+        StartUI.editItem(input2, trackerR);
+        edited = trackerR.findAll()[0];
+        Items expected = new Items("Fix Scanner");
+        assertThat(edited.getName(), is(expected.getName()));
+    }
+
+        @Test
+    public void whenDeleteItem() {
+            String[] answers1 = {"Fix Printer"};
+            Input input1 = new StubInput(answers1);
+            String[] answers2 = {"Fix Scanner"};
+            Input input2 = new StubInput(answers2);
+            Tracker tracker = new Tracker();
+            StartUI.createItem(input1, tracker);
+            StartUI.createItem(input2, tracker);
+            Items it = tracker.findAll()[0];
+            StartUI.deleteItem(input2, tracker);
+            it = tracker.findAll()[0];
+            Items expected = new Items("Fix Scanner");
+            assertThat(it.getName(), is(expected.getName()));
+
+        }
+
+    }
