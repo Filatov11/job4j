1a38a5c330ffbf279a875a0ab590616d8be14868
Filatov11 <denfilatov@protonmail.com>
2020-01-22 23:43:52 +0300

    5. Рефакторинг теста @Before @After[#178201]

--- chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -1,24 +1,45 @@
 package ru.job4j.strategy;
+//import org.junit.Test;
+//import java.io.ByteArrayOutputStream;
+//import java.io.PrintStream;
+///import static org.hamcrest.core.Is.is;
+//import static org.junit.Assert.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
+
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+
+
     @Test
     public void whenDrawSquare() {
         String newLine = System.getProperty("line.separator");
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
+        //this.loadOutput();
         new Paint().draw(new Square());
-        // проверяем результат вычисления
         assertThat(
-                new String(out.toByteArray()),
+                this.out.toString(),
                 is(
                         new StringBuilder()
                                 .append("********")
@@ -48,17 +69,10 @@ public class PaintTest {
     @Test
     public void whenDrawTriangle() {
         String newLine = System.getProperty("line.separator");
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
+        //this.loadOutput();
         new Paint().draw(new Triangle());
-        // проверяем результат вычисления
         assertThat(
-                new String(out.toByteArray()),
+                this.out.toString(),
                 is(
                         new StringBuilder()
                                 .append("   *    ")
