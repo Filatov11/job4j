d80f7c942455c12ef93aba7a4c5ee903b6c48821
Filatov11 <denfilatov@protonmail.com>
2020-01-28 23:45:29 +0300

    181780

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,7 +3,7 @@ import  java.util.Scanner;
 
 @SuppressWarnings("ALL")
 public class StartUI {
-     private String[] menuItems =  {"0.Add new Item", "1.Show all items", "2.Edit item", "3.Delete item", "4.Find item by Id", "5.Find items by name", "6.Exit Program" };
+    private String[] menuItems =  {"0.Add new Item", "1.Show all items", "2.Edit item", "3.Delete item", "4.Find item by Id", "5.Find items by name", "6.Exit Program" };
 
 
     public static void createItem(Input input, Tracker tracker) {
