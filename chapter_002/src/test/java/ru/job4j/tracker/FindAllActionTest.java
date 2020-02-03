ce131a369e3bf222222ae193bda5346bc69c0e0a
Filatov11 <denfilatov@protonmail.com>
2020-02-03 20:35:22 +0300

    #178202_2

--- chapter_002/src/test/java/ru/job4j/tracker/FindAllActionTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/FindAllActionTest.java
@@ -20,7 +20,7 @@ public class FindAllActionTest {
         ShowAction act = new ShowAction();
         act.execute(new StubInput(new String[]{}), tracker);
         String expect = new StringJoiner(System.lineSeparator())
-                .add( items.getName() + "\n")
+                .add( items.getName() + System.lineSeparator())
                 .toString();
         assertThat(new String(out.toByteArray()), is(expect));
         System.setOut(def);
@@ -28,20 +28,21 @@ public class FindAllActionTest {
 
     @Test
     public void whenFindByName() {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ByteArrayOutputStream out1 = new ByteArrayOutputStream();
         PrintStream def = System.out;
-        System.setOut(new PrintStream(out));
+        System.setOut(new PrintStream(out1));
         Tracker tracker = new Tracker();
-        Items items1 = new Items("fix bug");
+        Items items1 = new Items("fix1bug");
         tracker.add(items1);
-        Items items2 = new Items("fix_bug");
+        Items items2 = new Items("fix2bug");
         tracker.add(items2);
         FindNameAction act = new FindNameAction();
-        act.execute(new StubInput(new String[]{"fix_bug"}), tracker);
+        act.execute(new StubInput(new String[]{"fix1bug"}), tracker);
+        System.out.println (items1.getName());
        String expect = new StringJoiner(System.lineSeparator())
-                .add( items1.getName() + "\n")
+                .add( items1.getName() + System.lineSeparator())
                .toString();
-       assertThat(new String(out.toByteArray()), is(expect));
+       assertThat(new String(out1.toByteArray()), is(expect));
         System.setOut(def);
 
     }
@@ -51,36 +52,38 @@ public class FindAllActionTest {
 
 
 
- //       @Test
- //   public void whenChecInit()  {
+        @Test
+    public void whenChecInit()  {
 
- //       ByteArrayOutputStream out = new ByteArrayOutputStream();
-//        PrintStream def = System.out;
-//        System.setOut(new PrintStream(out));
- //       Tracker tracker = new Tracker();
- //       Items items = new Items("fix bug");
- //       tracker.add(items);
- //       ShowAction act = new ShowAction();
+       ByteArrayOutputStream out2 = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out2));
+       Tracker tracker = new Tracker();
+        Items items = new Items("fix bug");
+       tracker.add(items);
+       ShowAction act = new ShowAction();
 
-  //      StartUI startUI = new StartUI();
-  //      UserAction[] acts = {
-   //             new CreateAction()
-   //             , new ShowAction()
-   //             , new FindIdAction()
-  //              , new ReplaceAction()
-  //              , new DeleteAction()
-   //             , new FindIdAction()
-  //              , new FindNameAction()
-  //              , new ExitAction()
- //       };
-//        startUI.init( new StubInput(new String),  tracker, acts);
-//        act.execute(new StubInput(new String[]{}), tracker);
- //       String expect = new StringJoiner(System.lineSeparator())
-  //              .add( acts+ "\n")
- //               .toString();
- //       assertThat(new String(out.toByteArray()), is(expect));
- //       System.setOut(def);
-//    }
+        StartUI startUI = new StartUI();
+        UserAction[] acts = {
+                new CreateAction()
+                , new ShowAction()
+                , new FindIdAction()
+               , new ReplaceAction()
+               , new DeleteAction()
+                , new FindIdAction()
+                , new FindNameAction()
+               , new ExitAction()
+        };
+        String [] answ = {"=== Create a new Item ====", "=== Delete item ===="};
+       startUI.init( new StubInput(answ),  tracker, acts);
+      StubAction stubAction  = new StubAction();
+            stubAction.execute(new StubInput(new String[]{}), tracker);
+        String expect = new StringJoiner(System.lineSeparator())
+                .add( answ[0].toString() + System.lineSeparator())
+               .toString();
+        assertThat(new String(out2.toByteArray()), is(expect));
+        System.setOut(def);
+    }
 
 
 }
