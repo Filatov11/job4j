42203b7514df1689d2903111cf46f4a8396eb1c3
Filatov11 <denfilatov@protonmail.com>
2020-02-13 00:26:57 +0300

    0.3.Тестирование_исключений_с_junit#219366_1

--- chapter_002/src/main/java/ru/job4j/ex/FindEl.java
+++ chapter_002/src/main/java/ru/job4j/ex/FindEl.java
@@ -12,14 +12,19 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         boolean searchArrayResult = false;
-        for (String str : value){
-            rsl++;
-            if (str.equals(key)) {
-                searchArrayResult = true;
+
+
+        for (int i=0 ; i <= value.length-1; i++){
+            //rsl++;
+            if (value[i].equals(key)) {
+                rsl = i;
+              //  searchArrayResult = true;
+
                 break;
             }
         }
-        if (!searchArrayResult) { rsl=-1;}
+       // if (!searchArrayResult) { rsl=-1;}
+
         if ( rsl==-1) {
             throw new ElementNotFoundException("Element id not found");
         }
