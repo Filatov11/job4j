cbf1ea14a7ef7709be60226c82710a43a37f39b6
Filatov11 <denfilatov@protonmail.com>
2020-03-30 21:47:23 +0300

    LendingAutomat1

--- chapter_002/src/main/java/ru/job4j/sort/Machine.java
+++ chapter_002/src/main/java/ru/job4j/sort/Machine.java
@@ -1,17 +1,22 @@
 package ru.job4j.sort;
+
 import java.util.Arrays;
+
 public class Machine {
-    private  final int[] COINS={10,5,2,1};
+    private final int[] COINS = {10, 5, 2, 1};
+
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
-        int size = 0; int j = 0;
+        int size = 0;
+        int j = 0;
         int balance = money - price;
-        for (int i = 0 ;  i < COINS.length-1 ; i++ ) {
+        for (int i = 0; i < COINS.length - 1; i++) {
             while (balance >= COINS[i]) {
-             rsl[j] = COINS[i]; j++; balance = balance - COINS[i];
+                rsl[j++] = COINS[i];
+                balance = balance - COINS[i];
             }
         }
-        size = j ;
+        size = j;
         return Arrays.copyOf(rsl, size);
     }
 }
