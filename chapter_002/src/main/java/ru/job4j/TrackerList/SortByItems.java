8d067585b2b0ed15a5ef6edba43cb598ab3961a2
Filatov11 <denfilatov@protonmail.com>
2020-04-23 02:16:38 +0300

    #178169

--- chapter_002/src/main/java/ru/job4j/TrackerList/SortByItems.java
+++ chapter_002/src/main/java/ru/job4j/TrackerList/SortByItems.java
@@ -4,9 +4,11 @@ import ru.job4j.collection.Job;
 
 import java.util.Comparator;
 
-public class SortByItems { // implements Comparator<Job> {
-  //  @Override
-  //  public  int compare(Item first, Item second) {
- //       return first.getName().compareTo(second.getName());
- //   }
+public class SortByItems  implements Comparator<Item> {
+   @Override
+   public int compare(Item item1, Item item2) {
+       return item1.getName().compareTo(item2.getName());
+
+
+   }
 }
