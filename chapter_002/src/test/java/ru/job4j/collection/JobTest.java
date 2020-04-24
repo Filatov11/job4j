19bcfd6b881e607b33391a54f0ad6d6ba71e2fd3
Filatov11 <denfilatov@protonmail.com>
2020-04-25 01:47:52 +0300

    #178168

--- chapter_002/src/test/java/ru/job4j/collection/JobTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/JobTest.java
@@ -3,8 +3,7 @@ package ru.job4j.collection;
 import org.junit.Test;
 import java.util.Comparator;
 
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 public class JobTest {
     @Test
@@ -29,5 +28,50 @@ public class JobTest {
         assertThat(rsl1, greaterThan(0));
     }
 
+
+    @Test
+    public void whenCompatorJobDescByName() {
+        Comparator<Job> descByName = new JobDescByName();
+        int rsl2 = descByName.compare(
+                new Job("Impl task2", 0),
+                new Job("Fix bug2", 1)
+        );
+        assertThat(rsl2, lessThan(0));
+    }
+
+
+
+    @Test
+    public void whenCompatorJobDescByPriority() {
+        Comparator<Job>  descByPriority = new  JobDescByPriority();
+        int rsl3 = descByPriority.compare(
+                new Job("Impl task3", 0),
+                new Job("Fix bug3", 2)
+        );
+        assertThat(rsl3,  	greaterThan(0));
+    }
+
+
+
+    @Test
+    public void whenCompatorJobAscByName() {
+        Comparator<Job>  ascByName = new  JobAscByName();
+        int rsl4 = ascByName.compare(
+                new Job("Impl task4", 0),
+                new Job("Fix bug4", 2)
+        );
+        assertThat(rsl4,  	greaterThan(0));
+    }
+
+
+    @Test
+    public void whenCompatorJobAscByPrior() {
+        Comparator<Job>  ascByPrior = new  JobAscByPrior();
+        int rsl5 = ascByPrior.compare(
+                new Job("Impl task4", 0),
+                new Job("Fix bug4", 2)
+        );
+        assertThat(rsl5,  	lessThan(0));
+    }
 
 }
