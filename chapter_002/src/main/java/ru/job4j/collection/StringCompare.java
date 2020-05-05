326e09675dfa3566838521b7f0ea42cf0f6b3be9
Filatov11 <denfilatov@protonmail.com>
2020-05-05 20:52:55 +0300

    #178168.7

--- chapter_002/src/main/java/ru/job4j/collection/StringCompare.java
+++ chapter_002/src/main/java/ru/job4j/collection/StringCompare.java
@@ -9,8 +9,6 @@ public class StringCompare implements Comparator<String> {
     public int compare(String left, String right) {
         int lengthString = Math.min(left.length(), right.length());
         int val = Integer.compare(left.length(), right.length());
-        char leftChar;
-        char rightChar;
         for (int i = 0; i < lengthString; i++) {
             if (left.charAt(i) != right.charAt(i)) {
                 val = Character.compare(left.charAt(i), right.charAt(i));
@@ -22,17 +20,6 @@ public class StringCompare implements Comparator<String> {
     }
 
 
-
-
-    public static int compareChar(char one, char two) {
-        if (one == two) {
-            return 0;
-        } else {
-            return (one > two) ? 1 : -1;
-        }
-
-
-    }
 
 
 }
