37284237db0e7fc9325250521e669dea822d0f18
Filatov11 <denfilatov@protonmail.com>
2020-07-04 02:29:08 +0300

    24255#178051

--- chapter_002/src/main/java/ru/job4j/collection/JobSorter.java
+++ chapter_002/src/main/java/ru/job4j/collection/JobSorter.java
@@ -22,21 +22,16 @@ public class JobSorter {
         );
         Collections.sort(jobs, new JobDescByName().thenComparing(new JobDescByPriority()));
         System.out.println(jobs);
+
+        Comparator<Job> compareName = Comparator.comparing(Job::getName);
+        Comparator<Job> comparePriority = Comparator.comparingInt(Job::getPriority);
+        Comparator<Job> combine = compareName.thenComparing(comparePriority);
+        jobs.sort(combine);
+
     }
 
 
 
 
-// public static void main (String[] args) {
- //    List<Job> jobs =  Arrays.asList(
- //            new  Job("Fix bugs",4),
- //            new  Job("Impl tasks",2),
- //            new  Job("Reboot  server",1)
-//     );
- //   System.out.println(jobs);
- //   Collections.sort(jobs);
- //   System.out.println(jobs);
-//    Collections.sort(jobs, new SortByNameJob());
- //   System.out.println(jobs);
- //}
+
 }
--- chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,5 +1,6 @@
 package ru.job4j.search;
 import  java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
 private ArrayList<Person> persons = new ArrayList<Person>();
@@ -7,15 +8,29 @@ public void  add(Person person) {
     this.persons.add(person);
   }
     public ArrayList<Person> find(String key) {
+        Predicate<Person> combine =  t -> t.getName().contains(key) ||
+                    t.getSurname().contains(key) || t.getAddress().contains(key)
+                   || t.getPhone().contains(key);
         ArrayList<Person> result = new ArrayList<Person>();
-        for (Person i: persons) {
-            if  ((i.getSurname().contains(key)) || (i.getName().contains(key))
-                    || (i.getPhone().contains(key)) ||  (i.getAddress().contains(key))) {
-                result.add(i);
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
             }
-           }
+        }
         return result;
         }
+
+
+ //   public ArrayList<Person> find(String key) {
+   //     ArrayList<Person> result = new ArrayList<Person>();
+  //      for (Person i: persons) {
+  ///          if  ((i.getSurname().contains(key)) || (i.getName().contains(key))
+  //                  || (i.getPhone().contains(key)) ||  (i.getAddress().contains(key))) {
+    //            result.add(i);
+   //         }
+   //     }
+//        return result;
+ //   }
 
 
     }
