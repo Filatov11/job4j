fdeef2b095a8eb99577723ba96a724666dca9a3f
Filatov11 <denfilatov@protonmail.com>
2020-07-04 01:03:36 +0300

    70608#178053.1

--- chapter_002/src/main/java/ru/job4j/lambda/FunctionCount.java
+++ chapter_002/src/main/java/ru/job4j/lambda/FunctionCount.java
@@ -2,7 +2,6 @@ package ru.job4j.lambda;
 import java.util.*;
 import java.util.function.Function;
 
-
 public class FunctionCount {
 
     @FunctionalInterface
@@ -10,40 +9,19 @@ public class FunctionCount {
         List<Double> func(int x, int y);
     }
 
-
-
- static    List<Double> diapason(int start, int end, IFunc func) {
-     List<Double> list=new ArrayList<Double>();
-
+ static    List<Double> diapason(int start, int end, Function < Double, Double > func) {
+     List<Double> list = new ArrayList<>();
+     double val;
+     for (int i = start; i < end ; i++) {
+               val =  func.apply((double) i);
+                  list.add(val);
+             }
         return list;
     }
 
-  double  func = (x) -> {x};
 
-    //  List<Double> diapason(int start, int end, Function<Double, Double> func);
-    public static void main(String[] args) {
+  //  public static void main(String[] args) {
+ //   }
 
-    }
-//   public static List<Double> diapason(int start, int end, IMyFunc myFunc func) {
-  //     List<Double> list=new ArrayList<Double>();
-  //    double val;
-  //     for (int i = start; i < end ; i++) {
- //          val = utilMeth(myFunc, i);
- //          list.add(val);
- //      }
- //      return list;
-//   }
-
- //   IMyFunc myFunc = (str) -> {
- //       int c = 0;
- //       char ch[]= new char[str.length()];
-  //      for(int i = 0; i < str.length(); i++){
-  //          ch[i] = str.charAt(i);
- //           if(((i > 0) && (ch[i] != ' ') && (ch[i-1] == ' ')) ||
- //                   ((ch[0] != ' ') && (i == 0)))
- //               c++;
- //       }
- //       return c;
- //   };
 
 }
--- chapter_002/src/test/java/ru/job4j/lambda/FunctionCountTest.java
+++ chapter_002/src/test/java/ru/job4j/lambda/FunctionCountTest.java
@@ -14,14 +14,14 @@ public class FunctionCountTest {
     }
     @Test
     public void whenQuadFunctionThenQuadResults() {
-        List<Double> result = FunctionCount.diapason(5, 8, x -> x * x + 1);
-        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        List<Double> result = FunctionCount.diapason(1, 4, x -> x * x + 1);
+        List<Double> expected = Arrays.asList(2D, 5D, 10D);
         assertThat(result, is(expected));
     }
     @Test
     public void whenExponFunctionThenExponResults() {
-        List<Double> result = FunctionCount.diapason(5, 8, x -> 2 ^ x + 1);
-        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        List<Double> result = FunctionCount.diapason(1, 4, x ->  Math.pow(2, x)+ 1);
+        List<Double> expected = Arrays.asList(3D, 5D, 9D);
         assertThat(result, is(expected));
     }
 }
