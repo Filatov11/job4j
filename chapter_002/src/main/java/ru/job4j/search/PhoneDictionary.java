c7233a7a995fc69431f9dca71fdfd8b5aafa585e
Filatov11 <denfilatov@protonmail.com>
2020-07-05 02:48:18 +0300

    24255#178051.1

--- chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -2,16 +2,26 @@ package ru.job4j.search;
 import  java.util.ArrayList;
 import java.util.function.Predicate;
 
+
 public class PhoneDictionary {
 private ArrayList<Person> persons = new ArrayList<Person>();
 public void  add(Person person) {
     this.persons.add(person);
   }
     public ArrayList<Person> find(String key) {
-        Predicate<Person> combine =  t -> t.getName().contains(key) ||
-                    t.getSurname().contains(key) || t.getAddress().contains(key)
-                   || t.getPhone().contains(key);
         ArrayList<Person> result = new ArrayList<Person>();
+        Predicate<Person> predicateName = t -> t.getName().contains(key);
+        Predicate<Person> predicateSurname = t -> t.getSurname().contains(key);
+        Predicate<Person> predicateAddress = t -> t.getAddress().contains(key);
+        Predicate<Person> predicatePhone = t -> t.getPhone().contains(key);
+        Predicate<Person> combine =
+                predicateName.or(predicateName.or(predicateSurname.or(predicateAddress.or(predicatePhone))));
+      //  result =
+
+    //    Predicate<Person> combine =  t -> t.getName().contains(key) ||
+       //             t.getSurname().contains(key) || t.getAddress().contains(key)
+       //            || t.getPhone().contains(key);
+   //     ArrayList<Person> result = new ArrayList<Person>();
         for (Person person : persons) {
             if (combine.test(person)) {
                 result.add(person);
@@ -31,6 +41,9 @@ public void  add(Person person) {
    //     }
 //        return result;
  //   }
+
+
+
 
 
     }
